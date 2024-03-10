41fe339e613882d05cb40659771ccf5cca55abbb
Muzaffar Ismatillayev <7029565934@my.lerna.uz>
2024-03-11 02:32:00 +0500

    'main'

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas
+import numpy
+
+
+def main():
+    print("Hello World!")
+    print("This is a test of the Python environment.")
+    print("This is a test of the Python environment.")
+
+    # Create a dataframe
+
+
+if __name__ == "__main__":
+    main()
+
